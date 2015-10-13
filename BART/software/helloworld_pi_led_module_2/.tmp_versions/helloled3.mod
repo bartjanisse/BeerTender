e876d8e3879b4b88ee8c089d189e2a5994818421
Bart Janisse <bartjanisse@hotmail.com>
2015-10-13 23:05:16 +0200

    Adde helloled3 module which is a cleaned up helloled2 module

---
+++ BART/software/helloworld_pi_led_module_2/.tmp_versions/helloled3.mod
@@ -0,0 +1,2 @@
+/home/student/bart/programming/school/prt/BeerTender/BART/software/helloworld_pi_led_module_2/helloled3.ko
+/home/student/bart/programming/school/prt/BeerTender/BART/software/helloworld_pi_led_module_2/helloled3.o
---
+++ BART/software/helloworld_pi_led_module_2/helloled3.h
@@ -0,0 +1,23 @@
+/*
+ * 11-10-2015 - Bart Janisse
+ */
+ 
+#ifndef _HELLOLED2_H_
+#define _HELLOLED2_H_
+
+#define DEVICE_NAME "helloled3"	/* Device name as it appears in /proc/devices   */
+#define BUF_LEN 	2			/* Max length of the message from the device */
+#define SUCCESS 	0
+#define GPIO_LED	14			/* GPIO 14 is connected to pen 8 of the Raspberry connector */
+
+/* 
+ * Function prototypes
+ */
+int init_module(void);
+void cleanup_module(void);
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+#endif
